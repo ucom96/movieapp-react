ecbf2ebe2d7f7457eaf87554813b4bf68bde3906
관리자 <gwanlija@joyunaui-MacBookAir.local>
2022-08-09 21:33:50 +0900

    useEffect

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,11 +1,24 @@
 import Button from "./component/Button";
 import styles from "./App.module.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [value, setValue] = useState(0);
+  const onClick = () => {
+    setValue((prev) => prev + 1);
+  };
+  //컴포넌트가 렌더될때마다 반복실행되지 않고 첫 렌더때만 실행되기를 필요로하는 코드도 있을 수 있음!
+  //useEffect()를 사용할것!
+  console.log("it renders all time");
+  const unChange = () => {
+    console.log("it renders only one time");
+  };
+  useEffect(unChange, []);
   return (
     <div>
       <h1 className={styles.title}>Hello ReactJS</h1>
-      <Button text="Hello" />
+      <div>{value}</div>
+      <Button text="click me" onClick={onClick} />
     </div>
   );
 }
--- react-for-beginners/src/component/Button.js
+++ react-for-beginners/src/component/Button.js
@@ -1,6 +1,17 @@
 import styles from "./Button.module.css";
-function Button({ text }) {
-  return <button className={styles.btn}>{text}</button>;
+import PropTypes from "prop-types";
+
+function Button({ text, onClick }) {
+  return (
+    <button onClick={onClick} className={styles.btn}>
+      {text}
+    </button>
+  );
 }
+
+//propTypes (o) PropTypes (x)
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+};
 
 export default Button;
